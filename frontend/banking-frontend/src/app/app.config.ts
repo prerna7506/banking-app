bbfca5cad065ddb2ecef740cafc12537bc27ce3c
Prerna <demo@gmail.com>
2025-09-08 23:09:50 +0530

    feat(frontend):intsall NGRx 19 and configure root store+devtools

---
+++ frontend/banking-frontend/src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideRouterStore } from '@ngrx/router-store';
+import { provideEffects } from '@ngrx/effects';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }), provideRouterStore(), provideEffects()]
+};
